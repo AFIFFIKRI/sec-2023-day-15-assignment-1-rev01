3c4dc0bcf30b76f2c841dd58f6d6c9ebf8cf3c11
AFIFFIKRIecho <afiffikri_94@yahoo.com>
2023-04-18 13:46:07 +0800

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/sec-2023-day-15-assignment-1-rev01",
+  
   plugins: [react()],
+  base: "/sec-2023-day-15-assignment-1-rev01/",
   server: {
     watch: {
       usePolling: true,
