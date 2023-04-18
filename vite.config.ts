2bfbe9d34e2d51f6dc5002886e28b7e0aa11d1f6
AFIFFIKRIecho <afiffikri_94@yahoo.com>
2023-04-18 13:23:09 +0800

    update gh pages react

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/sec-2023-day-15-assignment-1-rev01",
   plugins: [react()],
   server: {
     watch: {
